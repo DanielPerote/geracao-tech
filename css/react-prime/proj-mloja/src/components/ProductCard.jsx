cb8c513fe63d903d37a8fdffbc0acbc4b5929729
DanielPerote <dr853364@gmail.com>
2026-01-29 21:11:00 -0300

    criando nova atividade react

---
+++ css/react-prime/proj-mloja/src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+const ProductCard = () => {
+    return (
+        <>
+            <section>
+                <h1>Lista de produtos</h1>
+                <ul>
+                    <li>
+                        
+                    </li>
+                </ul>
+            </section>
+        </>
+    );
+}
+ 
+export default ProductCard;
